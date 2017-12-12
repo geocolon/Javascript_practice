da0b99fb58d48b7e729e6b71a55adc818cbba8be
George Colon <geocolon@gmail.com>
2017-12-11 19:01:16 -0800

    update_12-11-2017

--- sudo_code.js
+++ sudo_code.js
@@ -8,13 +8,31 @@ END PROGRAM
 var array = [3,-1,-1];
 
 function mostFrequent(arr){
-    var storage = {};
-    for (i = 0; i < arr.length; i++){
-      for (val in storage) {
-      storage[arr[i]] = storage[val+1];
-      console.log(storage);
+  var storage = [];
+  for (i = 0; i < arr.length; i++){
+    for (j = 0; j < storage.length; j++){
+      if (arr[i] === storage[j]){
+
+      }
     }
   }
 }
 
+mostFrequent(array);
+
+// Working on new idea below
+var array = [3,-1,-1];
+
+function mostFrequent(arr){
+  var storage = {};
+  for (i = 0; i < arr.length; i++){
+    // console.log(storage[arr[i]] = 1);
+    for (var key in storage){
+       if (key === arr[i]){
+         storage = storage[key] + 1;
+       }
+     }
+    }
+     console.log(storage);
+}
 mostFrequent(array);
